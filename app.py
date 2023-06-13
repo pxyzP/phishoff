de03cf630170ef2f2ab30b927b2ceffef656ebc7
pxyzP <121533775+pxyzP@users.noreply.github.com>
2023-06-13 14:32:40 +0700

    Add files via upload
    final version

--- app.py
+++ app.py
@@ -1,18 +1,76 @@
 import streamlit as st
-st.set_page_config(page_title="PhishOff")
-import pandas as pd
 import numpy as np
-df = pd.DataFrame({
-     'first column' : [1,2,3,4],
-     ' second column': [10,20,30,40]
+import tensorflow as tf
+import re
+from transformers import BertTokenizer
 
-})
+@st.cache(allow_output_mutation=True)
+def get_model():
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = tf.saved_model.load('C:/Users/Peach/OneDrive/Desktop/modelbert300000/modelbert')  
+    return tokenizer, model
 
-st.write("Hello welcome to Pleng's website!;)")
-st.write(df)
 
-chart_data = pd.DataFrame(
-    np.random.randn(20,3),
-    columns=['a','b','c'])
-st.line_chart(chart_data)
+def main():
+    st.title("Welcome to Phish-Off!")
+    tokenizer, model = get_model()
+    user_input = st.text_area('Enter your URL here...')
+    button = st.button("Analyze")
 
+    d = {
+        0: 'Safe',
+        1: 'Unsafe'
+    }
+
+    # Preprocessing function
+    def preprocess_link(link):
+        # Convert the link to lowercase
+        processed_link = link.lower()
+
+        # Apply necessary transformations or feature engineering based on the existing code
+        processed_link = re.sub(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', '', processed_link)
+        processed_link = re.sub(r'^https?:\/\/', '', processed_link)
+        processed_link = re.sub(r'www\.', '', processed_link)
+        processed_link = re.sub(r'\.(com|org|net|mil|edu|COM|ORG|NET|MIL|EDU)$', '', processed_link)
+
+        return processed_link
+
+    if user_input and button:
+        input = user_input
+        user_input = preprocess_link(input)
+
+        # Tokenize the input
+        inputs = tokenizer.encode_plus(
+            user_input,
+            add_special_tokens=True,
+            max_length=512,
+            padding='max_length',
+            truncation=True,
+            return_tensors='tf'
+        )
+
+        # Convert input tensors to numpy arrays
+        input_ids = inputs['input_ids'].numpy()
+        attention_mask = inputs['attention_mask'].numpy()
+        token_type_ids = inputs['token_type_ids'].numpy()
+
+        # Get the signature_def
+        signature_def = model.signatures["serving_default"]
+
+        # Run the model prediction
+        output = signature_def(
+            input_ids=tf.constant(input_ids),
+            attention_mask=tf.constant(attention_mask),
+            token_type_ids=tf.constant(token_type_ids)
+        )
+
+        # Print the output tensor keys
+        print(output.keys())
+
+        # Perform the prediction using the correct output key
+        y_pred = np.argmax(output['logits'].numpy(), axis=1)
+        st.write("Prediction:", d[y_pred[0]])
+
+
+if __name__ == "__main__":
+    main()
